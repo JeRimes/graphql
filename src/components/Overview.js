dddfaeb9298759afd74bf03583859bed4be2cb53
Jeremy <jeremy@kidiwe.com>
2021-10-29 12:58:11 +0200

    save

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -25,39 +25,14 @@ query{
 `;
 
 export default function DivOverview() {
-    const { loading, error, data } = useQuery(QUERY);
-    if (loading) return <p>loading</p>;
-    if (error) return <p>error</p>;
+    // const { loading, error, data } = useQuery(QUERY);
+    // if (loading) return <p>loading</p>;
+    // if (error) return <p>error</p>;
+
+
     return (
         <div>
-            <p>{data.user.login}</p>
-
-            <div className="block-total-activity">
-                <img src={data.user.avatarUrl} alt="img-user-git" />
-                <div className="grey">
-                    <p>Commits</p>
-                    <p>{data.user.commitComments.totalCount}</p>
-                </div>
-                <div className="grey">
-                    <p>Repos</p>
-                    <p>0</p>
-                </div>
-                <div className="grey">
-                    <p>Lines of code</p>
-                    <p>0</p>
-                </div>
-                <div className="green-light">
-                    <p>Followers</p>
-                    <p>{data.user.following.totalCount}</p>
-                </div>
-                <div className="green-light">
-                    <p>Following</p>
-                    <p>{data.user.following.totalCount}</p>
-                </div>
-                <div className="green">
-                    <a href="/">Refresh</a>
-                </div>
-            </div>
+            <canvas id="bar-chart" width="800" height="450"></canvas>
         </div>
     )
 }
